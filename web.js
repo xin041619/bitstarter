275ce9d76a4c924692539998cebf9bec388ca6b9
xinxin <xinxin.wang.1@gmail.com>
2013-07-19 04:25:24 +0000

    update web.js to return index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response_buffer = fs.readFileSync("./index.html");
+  response.send(response_buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
