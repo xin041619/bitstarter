2d1cc7de0bbb44db51352731818185948be6a179
xinxin <xinxin.wang.1@gmail.com>
2013-07-28 01:27:12 +0000

    add carousel WIP

--- web.js
+++ web.js
@@ -3,8 +3,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+//statis resources
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function(request, response) {
   response_buffer = fs.readFileSync("./index.html");
+  response.send(response_buffer.toString('utf-8'));
+});
+
+app.get('/test/', function(request, response) {
+  response_buffer = fs.readFileSync("./test.html");
   response.send(response_buffer.toString('utf-8'));
 });
 
